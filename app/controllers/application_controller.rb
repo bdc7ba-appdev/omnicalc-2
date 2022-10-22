0b4c07670a2c78fb03786c471eaa6dcefe26e767
bdc7ba <bcrozier@chicagobooth.edu>
2022-10-22 21:18:27 +0000

    task 1 done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 
+    def add
+      render({:template => "calculation_templates/add.html.erb"})
+    end 
+
+    def multiply
+      render({:template => "calculation_templates/multiply.html.erb"})
+    end 
+
+    def divide
+      render({:template => "calculation_templates/divide.html.erb"})
+    end 
+
+    def subtract
+      render({:template => "calculation_templates/subtract.html.erb"})
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get("/", :controller => "application", :action => "add")
+
+get("/add", :controller => "application", :action => "add")
+
+get("/multiply", :controller => "application", :action => "multiply")
+
+get("/divide", :controller => "application", :action => "divide")
+
+get("/subtract", :controller => "application", :action => "subtract")
+
+
 end
